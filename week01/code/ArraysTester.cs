92e3de1e03fbec450f41f0318f7ad224da1fbc8a
SpencerSedano <spencer.sv20@gmail.com>
2024-06-12 20:41:10 +0800

    arrayTester

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.Runtime;
+
 public static class ArraysTester {
     /// <summary>
     /// Entry point for the tests
@@ -12,16 +15,21 @@ public static class ArraysTester {
         multiples = MultiplesOf(-2, 10);
         Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{-2, -4, -6, -8, -10, -12, -14, -16, -18, -20}
 
+
         Console.WriteLine("\n=========== PROBLEM 2 TESTS ===========");
+
         List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         RotateListRight(numbers, 1);
         Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{9, 1, 2, 3, 4, 5, 6, 7, 8}
+       
         numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         RotateListRight(numbers, 5);
         Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        
         numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         RotateListRight(numbers, 3);
         Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{7, 8, 9, 1, 2, 3, 4, 5, 6}
+        
         numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         RotateListRight(numbers, 9);
         Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{1, 2, 3, 4, 5, 6, 7, 8, 9}
@@ -41,7 +49,7 @@ public static class ArraysTester {
 
         // To my understanding, I have a number and a length, I need to multiply the
         // number by the length to get the last number.
-        // Then, I can reduce the number times the length until get 0
+        // Then, I can reduce the number times the length until I get 0
         // or I can do the opposite, starting at 0, and go up till the number given by number * length
 
         // Creating my array of doubles
@@ -109,6 +117,25 @@ public static class ArraysTester {
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // I have a list of integers and an integer as parameters (data, amount)
+
+        
+        amount %= data.Count;
+        if (amount == 0)
+        {
+            return;
+        } 
+
+        // Getting the correct position to get the range
+        int startIndex = data.Count - amount;
+        List<int> sublist = data.GetRange(startIndex, amount);
+
+        // Insert the sublist at the beginning of the original list
+        data.InsertRange(0, sublist);
+
+        // Delete the remainder elements of the list (I am removing the original elements of sublist because I already added the sublist at the beginning)
+        data.RemoveRange(startIndex, amount);
 
     }
 }
